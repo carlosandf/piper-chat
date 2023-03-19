b24e71148d0b3504ca7d467802b7158d00b6556f
carlosandf <carlos.andres.ff2912@gmail.com>
2023-03-18 21:42:52 -0500

    Using login and refreshAccessToken controllers

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { AuthController } from '../controllers/index.js';
+import { AuthController } from '../controllers/index.js'; // importar los controladores de autenticación
 
 const router = Router();
 
 router.post('/auth/register', AuthController.register);
+router.post('/auth/login', AuthController.login);
+router.post('/auth/refresh-access-toker', AuthController.refreshAccessToken);
 
 export const authRoutes = router;
