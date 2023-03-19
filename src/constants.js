a50a591f242fe967dcd946fd329bf9cb89b3ac77
carlosandf <carlos.andres.ff2912@gmail.com>
2023-03-18 21:48:48 -0500

    adding secret key for jsowebtoken

--- src/constants.js
+++ src/constants.js
@@ -5,3 +5,4 @@ dotenv.config();
 export const IP_SERVER = 'localhost';
 export const PORT = process.env.PORT || 3001;
 export const MONGO_URI = process.env.MONGO_URI;
+export const SECRET_KEY = process.env.SECRET_KEY;
