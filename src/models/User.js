ed32296b40cc94c4d3fb3f98a7d666e74d6a7bab
carlosandf <carlos.andres.ff2912@gmail.com>
2023-03-18 21:47:21 -0500

    removing the password in the response and hashing it before saving to bd

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 import { Schema, model } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-const UserSchema = Schema({
+const UserSchema = new Schema({
   email: {
     type: String,
     unique: true
@@ -16,6 +17,30 @@ const UserSchema = Schema({
   },
   avatar: {
     type: String
+  }
+});
+
+UserSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.password;
+  }
+});
+
+UserSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  try {
+    const saltRounds = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, saltRounds);
+
+    user.password = hash;
+  } catch (error) {
+    console.error(error);
+    next();
   }
 });
 
