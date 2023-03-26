7332e4aa0e4048286d8c7e43051ab095062ebd8a
carlosandf <carlos.andres.ff2912@gmail.com>
2023-03-25 22:19:18 -0500

    correcting error capturing

--- src/middlewares/authenticated.js
+++ src/middlewares/authenticated.js
@@ -1,24 +1,23 @@
 import { jwtServices } from '../utils/index.js';
 
 function asureAuth (req, res, next) {
-  const { headers } = req;
-  if (!headers.authorization) { // verificar que exista la propiedad 'authorization' en los headers
-    return res // responder con un 401 en caso de que no exista 'authorization'
-      .status(401)
-      .send({
-        message: 'No autorizado'
-      });
-  }
-
-  // separarar 'Bearer' del token -> 'Bearer <token>' -> ['Bearer', '<token>'].at(1) -> '<tokrn>'
-  const token = headers.authorization.split(' ').at(1);
-
   try {
+    const { headers } = req;
+    if (!headers.authorization) { // verificar que exista la propiedad 'authorization' en los headers
+      return res // responder con un 401 en caso de que no exista 'authorization'
+        .status(401)
+        .send({
+          message: 'No autorizado'
+        });
+    }
+
+    // separarar 'Bearer' del token -> 'Bearer <token>' -> ['Bearer', '<token>'].at(1) -> '<tokrn>'
+    const token = headers.authorization.split(' ').at(1);
+
     const hasExpired = jwtServices.hasExpiredToken(token); // verificar si el token ha expirado
 
     if (hasExpired) { // si expiró, enviamos el error al catch y finaliza la ejecución
-      const error = { message: 'Ivalid Token' };
-      throw error;
+      throw new Error('Ivalid Token');
     }
 
     const decodedToken = jwtServices.decoded(token); // decodificar el token
@@ -26,7 +25,7 @@ function asureAuth (req, res, next) {
 
     next(); // pasamos al siguinte endpoint
   } catch (error) { // capturar error
-    res.status(400).send(error);
+    res.status(400).send({ error: error.message });
   }
 }
 
