00d7e8dc7a5fbea3d46b455cf8e10404e88d8004
carlosandf <carlos.andres.ff2912@gmail.com>
2023-04-18 16:25:54 -0500

    Add documentation

--- src/routes/chat.router.js
+++ src/routes/chat.router.js
@@ -4,9 +4,9 @@ import { mdAuth } from '../middlewares/index.js';
 
 const router = Router();
 
-router.post('/chats', [mdAuth.asureAuth], ChatController.create);
-router.get('/chats', [mdAuth.asureAuth], ChatController.getAll);
-router.get('/chats/:id', [mdAuth.asureAuth], ChatController.getChat);
-router.delete('/chats/:id', [mdAuth.asureAuth], ChatController.deleteChat);
+router.post('/chats', [mdAuth.asureAuth], ChatController.create); // create new chat
+router.get('/chats', [mdAuth.asureAuth], ChatController.getAll); // get all chats
+router.get('/chats/:id', [mdAuth.asureAuth], ChatController.getChat); // get one chat
+router.delete('/chats/:id', [mdAuth.asureAuth], ChatController.deleteChat); // delete one chat
 
 export const chatRoutes = router;
